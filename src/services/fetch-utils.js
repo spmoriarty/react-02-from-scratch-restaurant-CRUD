95cb54c7f66999e513887cddceeb1984be4698bc
Sean Moriarty <spmoriarty1@gmail.com>
2022-07-01 11:49:13 -0700

    fetch utils built

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -18,5 +18,17 @@ export async function logout() {
     await client.auth.signOut();
 
     return window.location.href = '../';
-    
+
+}
+
+export async function createMenu(menu) {
+    const response = await client.from('food_menu').insert([menu]);
+}
+
+export async function getMenu() {
+    const response = await client.from('food_menu').select('*');
+}
+
+export async function getOrder(id) {
+    const response = await client.from('food_menu').select().match({ id }).single();
 }
