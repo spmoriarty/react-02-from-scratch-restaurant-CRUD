5582257019c71204542fc570b3423aa7cfac08bd
Sean Moriarty <spmoriarty1@gmail.com>
2022-07-01 12:17:46 -0700

    setting up authpage to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from 'react-router-dom';
+import { useEffect } from 'react/cjs/react.production.min';
+import AuthPage from './authPage';
+import { getUser } from './services/fetch-utils';
 
 
 export default function App() {
+const [user, setUser] = useState(null);
+
+
+  useEffect(() => {
+    async function fetch() {
+      const user = await getUser();
+      if (user) setUser();
+    }
+    fetch();
+  }, []);
+  
   return (
     <Router>
       <div className='App'>
         <Switch>
           <Route exact path='/'>
-            <HomePage />
+            { 
+              user 
+                ? <Redirect to='/menu'/> : <AuthPage setUser={setUser} />
+            }
           </Route>
           <Route exact path='/items/:id'> 
-            <Items />
+            {/* <Items /> */}
           </Route>
           <Route exact path='/create'>
-            <Create />
+            {/* <Create /> */}
           </Route>
         </Switch>
       </div>
--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,34 +1,34 @@
 import { client } from './client';
 
 export function getUser() {
-    return client.auth.session();
+  return client.auth.session();
 }
 
 export async function signUp(email, password) {
-    const response = await client.aught.signUp({ email, password });
-    return response.user;
+  const response = await client.aught.signUp({ email, password });
+  return response.user;
 
 }
 export async function signIn(email, password) {
-    const response = await client.auth.signIn({ email, password });
-    return response.user;
+  const response = await client.auth.signIn({ email, password });
+  return response.user;
 }
 
 export async function logout() {
-    await client.auth.signOut();
+  await client.auth.signOut();
 
-    return window.location.href = '../';
+  return window.location.href = '../';
 
 }
 
 export async function createMenu(menu) {
-    const response = await client.from('food_menu').insert([menu]);
+  const response = await client.from('food_menu').insert([menu]);
 }
 
 export async function getMenu() {
-    const response = await client.from('food_menu').select('*');
+  const response = await client.from('food_menu').select('*');
 }
 
 export async function getOrder(id) {
-    const response = await client.from('food_menu').select().match({ id }).single();
+  const response = await client.from('food_menu').select().match({ id }).single();
 }
